05e447bd22da0e34a300f0aab6f14269857f3fcb
Pablo Echevarria <pablohe@usario-Satellite-L305.(none)>
2013-07-21 16:40:52 -0300

    tp 1 sin comitear

--- multiplicacion_vectores/main.cu
+++ multiplicacion_vectores/main.cu
@@ -27,12 +27,18 @@ int main(int argc, char *argv[])
 
 	// Declare/aloque tres vectores D1, D2 y D3 de tamanio N en la memoria de la GPU
 	// SOLUCION:
+    thrust::device_vector<float> D1(N);
+    thrust::device_vector<float> D2(N);
+    thrust::device_vector<float> D3(N);
 
 	// llenar todo D1 con "0.0001 0.0001 ... 0.0001" 
 	// SOLUCION:
+    thrust::fill(D1.begin(), D1.end(), 0.0001);
 
 	// llenar todo D2 con "0.1 1.1 2.1 ..." 
 	// SOLUCION:
+	
+    thrust::sequence(D2.begin(), D2.end(), 0.1);
 
 	/* ---- Start ---- */
 	// un timer para GPU o CPU-multicore
@@ -44,6 +50,8 @@ int main(int argc, char *argv[])
 	{
 	// busque y aplique el algoritmo de thrust para multiplicar en la GPU: D3 = D1*D2
 	// SOLUCION:
+	thrust::transform(D1.begin(), D1.end(), D2.begin(), D3.begin(),
+                   thrust::multiplies<float>());
 
 	}
 
